3f06fff8ce127f4237adb0c3c670b797441eea89
Sergio Sepulveda <contacto@penti2s.com>
2022-05-10 15:03:43 -0400

    Creacion de tarjetas con la informacion dada por el formulario

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,9 +8,8 @@ document.querySelector('#btnRegistrar').addEventListener('click', async () =>{
     let raza = document.querySelector('#raza').value
     let previewElement = document.querySelector('#preview')
     let img = previewElement.style.backgroundImage;
-    let imgSrc = img.slice(5, img.lenght - 2)
-    let ki = document.querySelector('#poderPelea').value
-
+    let imgSrc = img.slice(4, -1).replace(/"/g, "")
+    let ki = document.querySelector('#poderPelea').value 
 
     let nuevoParticipante;
     if(raza === 'Saiyajin'){
@@ -18,7 +17,28 @@ document.querySelector('#btnRegistrar').addEventListener('click', async () =>{
     }else{
         nuevoParticipante = new Humano(nombre, imgSrc, ki, raza)
     }
-
     participantes.push(nuevoParticipante)
-    console.log(participantes)
+    reloadTable()
 })
+
+const reloadTable = () => {
+    const participantesTemplate = document.querySelector('#Participantes')
+    participantesTemplate.innerHTML = ''
+    console.log(participantes)
+    participantes.forEach((p,i) => {
+        participantesTemplate.innerHTML += `
+        <div class='px-3 pb-2 participante' data-fighter='${p.nombre}'>
+        <div class="card" style="width: 18rem;">
+        <img src="${p.img}" class="card-img-top" height='300px' alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${p.nombre}</h5>
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">Raza : ${p.raza}</li>
+          <li class="list-group-item">Poder : ${p.poder}</li>
+          <li class="list-group-item">i = ${i}</li>
+        </ul>
+      </div>`
+      console.log(p.img)
+    })
+}
